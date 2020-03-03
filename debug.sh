13d4a550e4134f7062ac8dab29c5f94ba6e3e85f
Joel Summerfield <52923935+NuclearRedeye@users.noreply.github.com>
2020-03-03 12:07:36 +0000

    fix: images not loading

--- debug.sh
+++ debug.sh
@@ -2,5 +2,6 @@
 mkdir -p ./debug/www
 cp -f ./dist/bin/* ./debug
 cp -rf ./dist/vfs/* ./src/html/* ./node_modules/@libero/texture/dist/* ./debug/www
+ln -s ../../reference-manuscripts/manuscripts ./debug/www/manuscripts
 cd ./debug
 node index.js
