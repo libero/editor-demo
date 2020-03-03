6a851da4ad9a36774bcd07c08652921ae1b03ef1
Joel Summerfield <52923935+NuclearRedeye@users.noreply.github.com>
2020-03-03 12:16:13 +0000

    fix: ignore if link already exists

--- debug-webpack.sh
+++ debug-webpack.sh
@@ -3,7 +3,7 @@ mkdir -p ./debug/www
 cp -f ./dist/bin/* ./debug
 cp -rf ./dist/vfs/* ./src/html/demo.js ./debug/www
 cp ./src/html/index-webpack.html ./debug/www/index.html
-ln -s ../../reference-manuscripts/manuscripts ./debug/www/manuscripts
+ln -sf ../../reference-manuscripts/manuscripts ./debug/www/manuscripts
 
 cd ./debug
 node index.js
