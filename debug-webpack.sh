d015a016073139c8262dcca8f7a8401a186102c0
NuclearRedeye <j.summerfield@elifesciences.org>
2020-03-06 14:42:07 +0000

    fix: symlinks created in submodule

--- debug-webpack.sh
+++ debug-webpack.sh
@@ -3,7 +3,10 @@ mkdir -p ./debug/www
 cp -f ./dist/bin/* ./debug
 cp -rf ./dist/vfs/* ./src/html/demo.js ./debug/www
 cp ./src/html/index-webpack.html ./debug/www/index.html
-ln -sf ../../reference-manuscripts/manuscripts ./debug/www/manuscripts
+if [ ! -e ./debug/www/manuscripts ];
+then
+  ln -s ../../reference-manuscripts/manuscripts ./debug/www/manuscripts
+fi
 
 cd ./debug
 node index.js
--- debug.sh
+++ debug.sh
@@ -2,7 +2,10 @@
 mkdir -p ./debug/www
 cp -f ./dist/bin/* ./debug
 cp -rf ./dist/vfs/* ./src/html/* ./node_modules/@libero/texture/dist/* ./debug/www
-ln -sf ../../reference-manuscripts/manuscripts ./debug/www/manuscripts
+if [ ! -e ./debug/www/manuscripts ];
+then
+  ln -s ../../reference-manuscripts/manuscripts ./debug/www/manuscripts
+fi
 
 cd ./debug
 node index.js
